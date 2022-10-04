043bcd7af8ed139dc0a6cbeac08f37020dff878f
Galina <gal-dej@yandex.ru>
2022-10-04 14:39:23 +0300

    Task has done

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,3 +3,18 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+void GetThirdFigure (int num)
+{
+    string str = num.ToString();
+    if(num>99)
+    {
+        Console.Write($"{num} -> ");
+        Console.WriteLine(str[2]);
+    }
+    else
+    {
+        Console.Write($"{num} -> ");
+        Console.WriteLine("третьей цифры нет");
+    }
+}
+GetThirdFigure(4563); 
